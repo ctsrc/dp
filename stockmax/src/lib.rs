f396f8e7ee1cfa8e214312c28543bfe7ab412a5b
Erik Nordstrøm <erik@nordstroem.no>
2021-07-22 01:13:26 +0200

    Add remaining tests. Our solver remains a stub as of yet and therefore these tests are currently failing. Our goal is to implement the solver :^)

--- stockmax/src/lib.rs
+++ stockmax/src/lib.rs
@@ -3,6 +3,14 @@
 /// ```
 /// assert_eq!(stockmax::stockmax(vec![5, 3, 2]), 0);
 /// ```
+///
+/// ```
+/// assert_eq!(stockmax::stockmax(vec![1, 2, 100]), 197);
+/// ```
+///
+/// ```
+/// assert_eq!(stockmax::stockmax(vec![1, 3, 1, 2]), 3);
+/// ```
 pub fn stockmax (prices: Vec<u32>) -> u64 {
   0
 }
